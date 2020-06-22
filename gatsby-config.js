e9f275d68f76c813ebd49613d6636f6c85581561
Philipp Siegmund <philipp@zauberware.com>
2020-06-22 22:18:36 +0200

    add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+const resolveConfig = require("tailwindcss/resolveConfig");
+const tailwindConfig = require("./tailwind.config.js");
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+module.exports = {
+  siteMetadata: {
+    title: `null6126`,
+    description: `Gatsby starter for null6126.de`,
+    author: `Philipp <me@null6126.de>`,
+  },
+  plugins: [
+    `gatsby-plugin-eslint`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-null6126`,
+        short_name: `null6126`,
+        start_url: `/`,
+        background_color: fullConfig.theme.colors.white,
+        theme_color: fullConfig.theme.colors.teal["400"],
+        display: `minimal-ui`,
+        icon: `src/images/tailwind-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(tailwindConfig),
+          require(`autoprefixer`),
+          ...(process.env.NODE_ENV === `production`
+            ? [require(`cssnano`)]
+            : []),
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
